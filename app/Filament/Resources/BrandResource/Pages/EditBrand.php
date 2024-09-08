665b7689e51beca8fb6bf70ed446ad54e6ed3bd2
yogandrn <yogaandrianp21@gmail.com>
2024-09-08 23:24:26 +0700

    crud category and brand

---
+++ app/Filament/Resources/BrandResource/Pages/EditBrand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\BrandResource\Pages;
+
+use App\Filament\Resources\BrandResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditBrand extends EditRecord
+{
+    protected static string $resource = BrandResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        // Redirect ke halaman index/list setelah create
+        return $this->getResource()::getUrl('index');
+    }
+}
---
+++ app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\CategoryResource\Pages;
+
+use App\Filament\Resources\CategoryResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCategory extends CreateRecord
+{
+    protected static string $resource = CategoryResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        // Redirect ke halaman index/list setelah create
+        return $this->getResource()::getUrl('index');
+    }
+}
